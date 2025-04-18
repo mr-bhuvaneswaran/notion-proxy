27cdaf54c094c6dc98e02af423b0aed16cdf30fd
Bhuvaneswaran <mr.bhuvaneswaran@gmail.com>
2025-04-18 18:02:35 +0530

    Add files via upload

---
+++ api/getPagePropertyItem.js
@@ -0,0 +1,28 @@
+
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const NOTION_TOKEN = process.env.NOTION_TOKEN;
+  const NOTION_VERSION = '2022-06-28';
+  const baseURL = 'https://api.notion.com/v1/pages';
+  const id = req.query.id || '';
+  const fullURL = id ? `${baseURL}/${id}` : baseURL;
+
+  try {
+    const response = await axios.get(
+      fullURL,
+      undefined,
+      {
+        headers: {
+          'Authorization': `Bearer ${NOTION_TOKEN}`,
+          'Notion-Version': NOTION_VERSION,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.error(err?.response?.data || err.message);
+    res.status(500).json({ error: 'Notion proxy error', detail: err?.response?.data });
+  }
+};
